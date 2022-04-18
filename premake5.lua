df8e8c7b49fed752a86615bd9064b8d736c225c1
Kristian Vines <kvines4@dxc.com>
2022-04-18 12:59:50 +1000

    premake tweak

--- premake5.lua
+++ premake5.lua
@@ -4,7 +4,7 @@ project "GLFW"
 	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	objdir ("obj/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
